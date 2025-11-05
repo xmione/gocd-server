cf418c393255f67d72fbf48f9a59c0e92d6147c5
xmione <solomiosisante@gmail.com>
2025-11-05 08:28:37 +0800

    Enhance error logging format in geterror.ps1 for better readability

--- Scripts/geterror.ps1
+++ Scripts/geterror.ps1
@@ -1,6 +1,10 @@
 Write-Host "Running Scripts/geterror.ps1..."
-Write-Host "gocd-server container errors:"
+Write-Host "====================================================================================" -ForegroundColor Yellow
+Write-Host "gocd-server container errors:" -ForegroundColor Yellow
+Write-Host "====================================================================================" -ForegroundColor Yellow
 docker logs gocd-server 2>&1 | Select-String -Pattern "(ERROR|Exception|failed|STDERR)" -CaseSensitive:$false -Context 3,5
 
-Write-Host "gocd-agent container errors:"
+Write-Host "====================================================================================" -ForegroundColor Yellow
+Write-Host "gocd-agent container errors:" -ForegroundColor Yellow
+Write-Host "====================================================================================" -ForegroundColor Yellow
 docker logs gocd-agent 2>&1 | Select-String -Pattern "(ERROR|Exception|failed|STDERR)" -CaseSensitive:$false -Context 3,5
