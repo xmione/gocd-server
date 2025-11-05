39d7e96f0a4841a1b6d09e5c0064a8bbb69f2a24
xmione <solomiosisante@gmail.com>
2025-11-05 08:23:29 +0800

    Updated script to display gocd-agent errors also.

--- Scripts/geterror.ps1
+++ Scripts/geterror.ps1
@@ -1,3 +1,6 @@
 Write-Host "Running Scripts/geterror.ps1..."
-Write-Host 'docker logs gocd-server 2>&1 | Select-String -Pattern "(ERROR|Exception|failed|STDERR)" -CaseSensitive:$false -Context 3,5'
+Write-Host "gocd-server container errors:"
 docker logs gocd-server 2>&1 | Select-String -Pattern "(ERROR|Exception|failed|STDERR)" -CaseSensitive:$false -Context 3,5
+
+Write-Host "gocd-agent container errors:"
+docker logs gocd-agent 2>&1 | Select-String -Pattern "(ERROR|Exception|failed|STDERR)" -CaseSensitive:$false -Context 3,5
